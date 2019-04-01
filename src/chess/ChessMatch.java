61e90bcae54793c3c21f4a65d2aabe663f0cece4
Frederico Benevides <fhfreitas@gmail.com>
2019-04-01 14:40:32 -0400

    Creation of BoardExceptions and defensive proggraming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,5 +24,6 @@ public class ChessMatch {
 	
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(6, 0));
 	}
 }
